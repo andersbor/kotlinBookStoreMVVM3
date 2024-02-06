d7d1a235e175d4ca877796ad11e11a0e6a273249
andersbor <anbo@zealand.dk>
2024-02-06 11:13:50 +0100

    gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
